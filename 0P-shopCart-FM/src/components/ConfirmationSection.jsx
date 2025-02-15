0b161b0ab5271125e4243cca5855dcc8d673386d
luisajcoy <luisacoy2005@gmail.com>
2025-02-14 20:52:53 -0500

    Confirmation section update

--- 0P-shopCart-FM/src/components/ConfirmationSection.jsx
+++ 0P-shopCart-FM/src/components/ConfirmationSection.jsx
@@ -1,35 +1,37 @@
-import React from 'react'
-import { useState } from 'react';
 import { useProducts } from '../hooks/useProducts';
+import "../components/ConfirmationSection.css";
+import { useState } from 'react';
+import React from 'react'
 
 export default function ConfirmationSection() {
 
-    // const [newOrder, setNewOrder] = useState(false);
     const {cart, totalCart,restartCart} = useProducts();
 
 
   return (
-    <div>
-      <img src="/src/assets/icon-order-confirmed.svg" alt="Order confirmed icon" />
-      <h1>Order Confirmed</h1>
-      <span>We hope you enjoy your food</span>
-      <ul>
+    <div className='confirmation-section'>
+      <img className='confirmation-icon' src="/src/assets/icon-order-confirmed.svg" alt="Order confirmed icon" />
+      <h1 className='section-title'>Order Confirmed</h1>
+      <span className='section-mesage'>We hope you enjoy your food</span>
+      <ul className='order-list'>
         {
             cart.map((item, index)=>{
                 return(
-                    <li key={index}>
-                        <img src={item.thumbnail} alt="thumbnail-img" />
-                        <strong>{item.name}</strong>
-                        <span>{item.quantity}x</span>
-                        <span>@ ${item.price}</span>
-                        <strong>${(item.price * item.quantity).toFixed(2)}</strong>
-                        <div>Order Total ${totalCart}</div>
-                        <button onClick={()=>restartCart()}>Start New Order</button>
+                    <li className='list-item-confirmed' key={index}>
+                        <img className='item-image' src={item.thumbnail} alt="thumbnail-img" />
+                        <div className='item-info'>
+                          <strong className='item-name'>{item.name}</strong>
+                          <span className='item-quantity'>{item.quantity}x</span>
+                          <span className='item-price'>@ ${(item.price).toFixed(2)}</span>
+                        </div>
+                        <strong className='item-price-total'>${(item.price * item.quantity).toFixed(2)}</strong>
                     </li>
                 )
-            })
-        }
+              })
+            }
+      <div className='total-cart'>Order Total <strong>${totalCart}</strong></div>
       </ul>
+      <button className='new-order-button' onClick={()=>restartCart()}>Start New Order</button>
     </div>
   )
 }
